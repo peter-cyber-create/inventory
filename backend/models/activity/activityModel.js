c22df8b18393d504eaa47885c23b0c0e20872309
Peter Cyber Create <peter@cyber-create.com>
2026-01-31 22:36:49 +0300

    Fix: Convert ES6 import to CommonJS require in activityModel.js
    - Changed import statement to require for consistency
    - Fixes ReferenceError: require is not defined in ES module scope
    - Resolves 502 Bad Gateway error

--- backend/models/activity/activityModel.js
+++ backend/models/activity/activityModel.js
@@ -1,4 +1,4 @@
-import { sequelize, DataTypes } from '../../config/db.js';
+const { sequelize, DataTypes } = require('../../config/db.js');
 const User = require('../users/userModel.js');
 
 const Activity = sequelize.define("activity", {
